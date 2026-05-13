be7c20ff39e4bafe099f79c91447b1cab4b987e2
luisbrustolin <71842762+luisbrustolin@users.noreply.github.com>
2026-05-13 22:54:38 +0000

    edicao#2

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -10,6 +10,24 @@ export default function Banner(){
             <button className="bg-green-500 px-4 py-2 rounded-lg hover:text-black m-4 " >Informações</button>
 
         </div>
+        <div className="md:grid grid-cols-2 items-center">
+        <div >
+            <img src="/img/css.svg" alt=""/>
+        </div>
+         <div >
+            <img src="/img/design.svg" alt=""/>
+        </div>
     </div>
+    </div>
+    
+        
+
+
+
+
+
+
+
+
     )
 }
